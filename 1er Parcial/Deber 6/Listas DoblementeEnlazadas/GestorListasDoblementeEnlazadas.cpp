a681ae8e645d6b38e9f32892d147339af75ca649
EscobarDavid <105328029+EscobarDavid@users.noreply.github.com>
2022-06-02 23:59:42 -0500

    Update GestorListasDoblementeEnlazadas.cpp

--- 1er Parcial/Deber 6/Listas DoblementeEnlazadas/GestorListasDoblementeEnlazadas.cpp
+++ 1er Parcial/Deber 6/Listas DoblementeEnlazadas/GestorListasDoblementeEnlazadas.cpp
@@ -1,3 +1,12 @@
+/*
+	Universidad de las Fuerzas Armadas "ESPE"
+	Programa que realize las acciones basicas para el manejo de una lista doblemente enlazada
+	Arias Xavier, Escobar David
+	Fecha de creacion: 01/06/2022
+	Fecha de modificacion: 02/06/2022
+	Grupo 5
+	https://github.com/EscobarDavid/Grupo5_4698_EstructuraDeDatos
+*/
 #include <iostream>
 #include "Menu.h"
 
